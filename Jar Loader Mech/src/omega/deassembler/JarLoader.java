7dda5e5199c76ca52e4c049ab7d9b223ccb15f60
omegaui <omegaui22@gmail.com>
2021-04-06 13:27:20 +0000

    ByteReader has been cloned to work like the default ByteReader

--- Jar Loader Mech/src/omega/deassembler/JarLoader.java
+++ Jar Loader Mech/src/omega/deassembler/JarLoader.java
@@ -1,21 +1,16 @@
 package omega.deassembler;
-import java.util.StringTokenizer;
-import java.util.jar.JarEntry;
-import java.util.Enumeration;
-import java.util.jar.JarFile;
-import java.net.URL;
-import java.io.File;
-import java.net.URLClassLoader;
-import java.util.LinkedList;
+import java.util.jar.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.lang.reflect.*;
 public class JarLoader {
 	public String jarPath;
-	public boolean module;
 	public LinkedList<ByteReader> readers = new LinkedList<>();
 	public LinkedList<String> classNames = new LinkedList<>();
 	public URLClassLoader loader;
-	public JarLoader(String jarPath, boolean module){
+	public JarLoader(String jarPath){
 		this.jarPath = jarPath;
-		this.module = module;
 		load();
 	}
 	private void load(){
@@ -42,9 +37,9 @@ public class JarLoader {
 	}
 	
 	private void loadClassNames(){
-		if(!module)
-			readJar();
+		readJar();
 	}
+     
 	public void readJar(){
 		try{
 			try(JarFile rtJarFile = new JarFile(jarPath)){
@@ -75,22 +70,22 @@ public class JarLoader {
 		zipPath = zipPath.substring(0, zipPath.length() - 1);
 		return zipPath.equals("module-info") ? null : zipPath;
 	}
-     public ByteReader getReader(String className){
-          for(ByteReader br : readers){
-               if(br.className.equals(className))
-                    return br;
-          }
-     	return null;
-     }
-     public Class loadClass(String className){
-     	try{
-     		return loader.loadClass(className);
-     	}
-     	catch(Exception e){ 
-     		System.err.println(e); 
-     	}
-          return null;
-     }
+	public ByteReader getReader(String className){
+		for(ByteReader br : readers){
+			if(br.className.equals(className))
+				return br;
+		}
+		return null;
+	}
+	public Class loadClass(String className){
+		try{
+			return loader.loadClass(className);
+		}
+		catch(Exception e){
+			System.err.println(e);
+		}
+		return null;
+	}
 	public void close(){
 		try{
 			loader.close();
@@ -104,7 +99,8 @@ public class JarLoader {
 	}
 	
 	public static void main(String[] args){
-		JarLoader loader = new JarLoader("/home/ubuntu/Documents/Omega IDE/build/Omega IDE.jar", false);
-		loader.getReader("omega.comp.ToggleListener").dataMembers.forEach(System.out::println);
+		JarLoader loader = new JarLoader("/home/ubuntu/Documents/Omega IDE/lib/openjfx-11.0.2_linux-x64_bin-sdk/javafx-sdk-11.0.2/lib/javafx.base.jar");
+		System.out.println(loader.getReader("javafx.util.Builder"));
+          loader.getReader("javafx.util.Builder").dataMembers.forEach(System.out::println);
 	}
 }
